c50b4f8bc7df6489c738b784b42da529d38672ff
Ally Preston <APreston@spartaglobal.com>
2019-08-29 15:07:33 +0100

    changed metadata file

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'All Rights Reserved'
 description 'Installs/Configures nginx'
 long_description 'Installs/Configures nginx'
 version '0.1.0'
-chef_version '>= 14.0'
+
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
