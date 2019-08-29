a5b92021653ba622773c5e94c3d42a2641eab08d
Ally Preston <APreston@spartaglobal.com>
2019-08-29 15:04:38 +0100

    set up new dev branch

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,13 +7,15 @@
 require 'spec_helper'
 
 describe 'nginx::default' do
-  context 'When all attributes are default, on Ubuntu' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
+  context 'When all attributes are default, on Ubuntu 16.04' do
+    let(:chef_run) do
+      # for a complete list of available platforms and versions see:
+      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
+      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner.converge(described_recipe)
+    end
 
-    platform 'ubuntu'
 
-    
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
